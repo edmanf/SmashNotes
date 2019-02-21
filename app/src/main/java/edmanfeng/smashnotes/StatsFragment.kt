36ec840acc9517ad005960b4187e2ea53e050f99
edmanf <edmanfeng@hotmail.com>
2019-02-21 15:21:54 -0800

    statsfragment interacts with gamespinner

--- app/src/main/java/edmanfeng/smashnotes/StatsFragment.kt
+++ app/src/main/java/edmanfeng/smashnotes/StatsFragment.kt
@@ -1,12 +1,18 @@
 package edmanfeng.smashnotes
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
@@ -20,6 +26,8 @@ class StatsFragment : Fragment() {
         fun newInstance() : StatsFragment {
             return StatsFragment()
         }
+
+        private lateinit var mGame : Game
     }
 
     lateinit var mGameViewModel : GameViewModel
@@ -31,15 +39,51 @@ class StatsFragment : Fragment() {
         mGameViewModel = ViewModelProviders
             .of(this)
             .get(GameViewModel::class.java)
+
+        val game = activity?.getPreferences(Context.MODE_PRIVATE)
+            ?.getString(SharedPrefs.GAME_SHARED_PREF_KEY, Game.SSBU.toString())
+        mGame = Game.valueOf(game ?: Game.SSBU.toString())
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val context = requireContext()
         val v = inflater.inflate(R.layout.stats_fragment, container, false)
+        val gameRecyclerView = v.findViewById<RecyclerView>(R.id.game_history_recyclerview)
+        val gameAdapter = GameAdapter(mGameViewModel.allGames.value)
+        gameRecyclerView.adapter = gameAdapter
+        val gameRecyclerViewManager = LinearLayoutManager(context)
+        gameRecyclerViewManager.stackFromEnd = true
+        gameRecyclerViewManager.reverseLayout = true
+        gameRecyclerView.layoutManager = gameRecyclerViewManager
+
         mRatingChart = v.findViewById(R.id.rating_chart)
         mGameViewModel.allGames.observe(this, Observer {
-            updateChart(it)
+            updateChart(mGameViewModel.getGame(mGame))
+            gameAdapter.setGames(mGameViewModel.getGame(mGame))
         })
 
+        val gameSpinner = v.findViewById<Spinner>(R.id.game_spinner)
+        val gameSpinnerAdapter = ArrayAdapter<String>(
+            context,
+            android.R.layout.simple_spinner_item,
+            Game.getAdapterList(null)
+        )
+
+
+        gameSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        gameSpinner.adapter = gameSpinnerAdapter
+        val pos = gameSpinnerAdapter.getPosition(mGame.toString())
+        gameSpinner.setSelection(pos)
+        gameSpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val item = parent?.getItemAtPosition(position) as String
+                gameAdapter.setGames(mGameViewModel.getGame(item))
+                updateChart(mGameViewModel.getGame(item))
+            }
+        }
+        val characterSpinner = v.findViewById<Spinner>(R.id.player_character_spinner)
 
         return v
     }
