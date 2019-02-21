8ba55b97cb3d0f7a3fed493ee513fb3688dc1af6
edmanf <edmanfeng@hotmail.com>
2019-02-21 15:20:20 -0800

    changed Game arraylist behavior to not require a placeholder option

--- app/src/main/java/edmanfeng/smashnotes/Game.kt
+++ app/src/main/java/edmanfeng/smashnotes/Game.kt
@@ -6,10 +6,13 @@ enum class Game {
     companion object {
         /**
          * Returns a List containing a String representation of all values of
-         * Game as well as a default value
+         * Game as well as a default value. If default is null, no value is added
          */
-        fun getAdapterList(default: String = "All") : List<String>{
-            val array = mutableListOf(default)
+        fun getAdapterList(default: String? = "All") : List<String>{
+            val array = mutableListOf<String>()
+            if (default != null) {
+                array.add(default)
+            }
             values().forEach { array.add(it.toString()) }
             return array
         }
