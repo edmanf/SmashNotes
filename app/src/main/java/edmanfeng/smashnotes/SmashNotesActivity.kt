44b048c85408dce2404fd26a365454e06877f3ba
edmanf <edmanfeng@hotmail.com>
2019-02-20 16:56:43 -0800

    Setup responses to nav drawer clicks. Now closes on item click

--- app/src/main/java/edmanfeng/smashnotes/SmashNotesActivity.kt
+++ app/src/main/java/edmanfeng/smashnotes/SmashNotesActivity.kt
@@ -1,16 +1,29 @@
 package edmanfeng.smashnotes
 
+import android.os.Bundle
+import android.util.Log
+import android.view.ContextMenu
 import android.view.MenuItem
+import android.view.View
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
 
 class SmashNotesActivity : SingleFragmentNavDrawerActivity() {
     override fun createFragment(): Fragment {
         return OverviewFragment.newInstance()
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            else -> super.onOptionsItemSelected(item)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
+        val navView = findViewById<NavigationView>(R.id.nav_view)
+        navView.setNavigationItemSelectedListener {
+            drawerLayout.closeDrawer(GravityCompat.START)
+            when(it.itemId) {
+                else -> true
+            }
         }
     }
 }
