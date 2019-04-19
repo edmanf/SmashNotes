ef859645adfd4f0a97a817fc05e7dc30ba6bf9c6
Edman Feng <edmanfeng@hotmail.com>
2019-04-19 11:57:52 -0700

    added default constructor

--- app/src/androidTest/java/edmanfeng/smashnotes/GameDatabaseInstrumentedTest.kt
+++ app/src/androidTest/java/edmanfeng/smashnotes/GameDatabaseInstrumentedTest.kt
@@ -14,6 +14,7 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import testUtil.LiveDataTestUtil.getValue
 import java.io.IOException
 
 
@@ -43,10 +44,78 @@ class GameDatabaseInstrumentedTest {
     }
 
     @Test
-    @Throws(Exception::class)
-    fun simpleWriteAndRead() {
-        val game = GameRecord(
-            0,
+    @Throws(IOException::class)
+    fun listDelete() {
+        val game1 = GameRecord(id = 6, notes = "NOTES")
+        val game2 = GameRecord(id = 27, playerCharacter = "Yoshi")
+        val game3 = GameRecord(id = 10, hazards = true)
+        gameDao.insert(game1)
+        gameDao.insert(game2)
+        gameDao.insert(game3)
+        assertThat(getValue(gameDao.getAll()).size, equalTo(3))
+        gameDao.delete(listOf(game3, game1, game2))
+        assert(getValue(gameDao.getAll()).isEmpty())
+
+        val game4 = GameRecord(id = 3, stage = "Final Destination")
+        gameDao.insert(game1)
+        gameDao.insert(game2)
+        gameDao.insert(game3)
+        gameDao.insert(game4)
+        assertThat(getValue(gameDao.getAll()).size, equalTo(4))
+        gameDao.delete(listOf(game1, game3))
+        var list = getValue(gameDao.getAll())
+        assertThat(list.size, equalTo(2))
+        assert(list.contains(game2))
+        assert(list.contains(game4))
+
+        gameDao.delete(listOf())
+        list = getValue(gameDao.getAll())
+        assertThat(list.size, equalTo(2))
+        assert(list.contains(game2))
+        assert(list.contains(game4))
+
+        gameDao.delete(listOf(game2))
+        list = getValue(gameDao.getAll())
+        assertThat(list.size, equalTo(1))
+        assert(list.contains(game4))
+
+        gameDao.delete(listOf(game1, game4))
+        list = getValue(gameDao.getAll())
+        assert(list.isEmpty())
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun singleDelete() {
+        val game1 = GameRecord(id = 5)
+        gameDao.insert(game1)
+        val game2 = GameRecord(id = 10)
+        gameDao.insert(game2)
+        var list = getValue(gameDao.getAll())
+        assertThat(list.size, equalTo(2))
+        assert(list.contains(game2))
+        assert(list.contains(game1))
+        gameDao.delete(game2)
+        list = getValue(gameDao.getAll())
+        assertThat(list.size, equalTo(1))
+        assert(list.contains(game1))
+
+        val game3 = GameRecord(id = 4, notes = "NOOO")
+        gameDao.insert(game3)
+        gameDao.delete(game1)
+        list = getValue(gameDao.getAll())
+        assertThat(list.size, equalTo(1))
+        assert(list.contains(game3))
+
+        gameDao.delete(game3)
+        assert(getValue(gameDao.getAll()).isEmpty())
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun insert() {
+        var game = GameRecord(
+            20,
             "b",
             "c",
             "d",
@@ -57,9 +126,29 @@ class GameDatabaseInstrumentedTest {
             "j",
             "k"
         )
+        assertThat(getValue(gameDao.getAll()).size, equalTo(0))
         gameDao.insert(game)
-        val list = gameDao.getAll().value
-        assertThat(list?.get(0), equalTo(game))
+        var list = getValue(gameDao.getAll())
+        assertThat(list.get(0), equalTo(game))
+        assertThat(list.size, equalTo(1))
+
+        val game2 = GameRecord(
+            25,
+            "c",
+            "d",
+            "e",
+            "f",
+            false,
+            "a",
+            20,
+            "fdksal",
+            "no"
+        )
+        gameDao.insert(game2)
+        list = getValue(gameDao.getAll())
+        assertThat(list.size, equalTo(2))
+        assertThat(list.get(0), equalTo(game))
+        assertThat(list.get(1), equalTo(game2))
 
     }
 }
--- app/src/main/java/edmanfeng/smashnotes/GameRecord.kt
+++ app/src/main/java/edmanfeng/smashnotes/GameRecord.kt
@@ -6,14 +6,14 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "game_records")
 data class GameRecord(
-    @PrimaryKey(autoGenerate = true) var id : Int = 0,
-    @ColumnInfo(name = "player_character") var playerCharacter: String,
-    @ColumnInfo(name = "opponent_character") var opponentCharacter: String,
-    @ColumnInfo(name = "opponent_tag") var opponentTag : String,
-    @ColumnInfo(name = "stage") var stage : String,
-    @ColumnInfo(name = "hazards") var hazards : Boolean,
-    @ColumnInfo(name = "result") var result : String,
-    @ColumnInfo(name = "gsp") var gsp : Int,
-    @ColumnInfo(name = "notes") var notes : String,
-    @ColumnInfo(name = "game") var game : String
+    @PrimaryKey(autoGenerate = true) var id : Int = 1,
+    @ColumnInfo(name = "player_character") var playerCharacter: String = "Mario",
+    @ColumnInfo(name = "opponent_character") var opponentCharacter: String = "Mario",
+    @ColumnInfo(name = "opponent_tag") var opponentTag : String = "John",
+    @ColumnInfo(name = "stage") var stage : String = "Battlefield",
+    @ColumnInfo(name = "hazards") var hazards : Boolean = false,
+    @ColumnInfo(name = "result") var result : String = "win",
+    @ColumnInfo(name = "gsp") var gsp : Int = 3500000,
+    @ColumnInfo(name = "notes") var notes : String = "",
+    @ColumnInfo(name = "game") var game : String = Game.SSBU.toString()
 )
