dd04e7b834d0db97a685ee6094970dc0987967b8
Edman Feng <edmanfeng@hotmail.com>
2019-06-10 01:15:42 -0700

    updated so tests work both locally and on device

--- app/src/sharedTest/java/edmanfeng/smashnotes/OverviewFragmentTest.kt
+++ app/src/sharedTest/java/edmanfeng/smashnotes/OverviewFragmentTest.kt
@@ -1,22 +1,34 @@
 package edmanfeng.smashnotes
 
+import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import edmanfeng.smashnotes.fragments.OverviewFragment
+import edmanfeng.smashnotes.fragments.OverviewFragmentDirections
 import org.junit.Test
+import org.junit.runner.RunWith
 import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
 
+@RunWith(AndroidJUnit4::class)
 class OverviewFragmentTest {
+
     @Test
     fun clickAddGameButton_navigateToMatchRecordFragment() {
         val navController = mock(NavController::class.java)
-        val scenario = launchFragmentInContainer<OverviewFragment>()
+
+        val scenario = launchFragmentInContainer<OverviewFragment>(Bundle(), R.style.AppTheme)
         scenario.onFragment {
             Navigation.setViewNavController(it.view!!, navController)
         }
-
         onView(withId(R.id.add_button)).perform(click())
-
+        verify(navController).navigate(
+            NavGraphDirections.actionGlobalMatchRecordFragment(GameRecord.NEW_GAME_ID)
+        )
     }
 }
