f552e9a0e93bdac84f8e53116d7e63d5918281ed
edmanf <edmanfeng@hotmail.com>
2019-06-13 15:52:35 -0700

    fixed migrations for version 2

--- app/src/main/java/edmanfeng/smashnotes/repo/GameDatabase.kt
+++ app/src/main/java/edmanfeng/smashnotes/repo/GameDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
 import edmanfeng.smashnotes.GameRecord
 
-@Database(entities = arrayOf(GameRecord::class), version = 4)
+@Database(entities = arrayOf(GameRecord::class), version = 2)
 @TypeConverters(Converters::class)
 abstract class GameDatabase : RoomDatabase() {
     abstract fun gameDao() : GameDao
@@ -21,7 +21,7 @@ abstract class GameDatabase : RoomDatabase() {
                     context.applicationContext,
                     GameDatabase::class.java,
                     "game_database"
-                ).addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4).build()
+                ).addMigrations(MIGRATION_1_2).build()
                 INSTANCE = instance // Can't return INSTANCE because it is private
                 return instance
             }
@@ -29,19 +29,9 @@ abstract class GameDatabase : RoomDatabase() {
 
         private val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE game_records ADD COLUMN group_mode TEXT")
-            }
-        }
-
-        private val MIGRATION_2_3 = object : Migration(2, 3) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE game_records ADD COLUMN game_type TEXT")
-            }
-        }
-
-        private val MIGRATION_3_4 = object : Migration(3, 4) {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.execSQL("ALTER TABLE game_records ADD COLUMN game_number INTEGER")
+                database.execSQL("ALTER TABLE game_records ADD COLUMN group_mode TEXT NOT NULL DEFAULT ${GameRecord.GroupMode.SESSION}")
+                database.execSQL("ALTER TABLE game_records ADD COLUMN game_type TEXT NOT NULL DEFAULT ${GameRecord.GameType.PRACTICE}")
+                database.execSQL("ALTER TABLE game_records ADD COLUMN game_number INTEGER NOT NULL DEFAULT 0")
             }
         }
     }
