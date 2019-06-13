ceae98fade3210e866ca9284ae578f6eddac5575
edmanf <edmanfeng@hotmail.com>
2019-06-12 17:07:20 -0700

    updating versions, not done. Need to add defaults and NOT NULL(I think)

--- app/src/main/java/edmanfeng/smashnotes/repo/GameDatabase.kt
+++ app/src/main/java/edmanfeng/smashnotes/repo/GameDatabase.kt
@@ -2,9 +2,11 @@ package edmanfeng.smashnotes.repo
 
 import android.content.Context
 import androidx.room.*
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import edmanfeng.smashnotes.GameRecord
 
-@Database(entities = arrayOf(GameRecord::class), version = 1)
+@Database(entities = arrayOf(GameRecord::class), version = 4)
 @TypeConverters(Converters::class)
 abstract class GameDatabase : RoomDatabase() {
     abstract fun gameDao() : GameDao
@@ -19,9 +21,27 @@ abstract class GameDatabase : RoomDatabase() {
                     context.applicationContext,
                     GameDatabase::class.java,
                     "game_database"
-                ).build()
+                ).addMigrations(MIGRATION_1_2, MIGRATION_2_3, MIGRATION_3_4).build()
                 INSTANCE = instance // Can't return INSTANCE because it is private
                 return instance
+            }
+        }
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE game_records ADD COLUMN group_mode TEXT")
+            }
+        }
+
+        private val MIGRATION_2_3 = object : Migration(2, 3) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE game_records ADD COLUMN game_type TEXT")
+            }
+        }
+
+        private val MIGRATION_3_4 = object : Migration(3, 4) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE game_records ADD COLUMN game_number INTEGER")
             }
         }
     }
