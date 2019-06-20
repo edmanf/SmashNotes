f683ef872686d4eee193d535ce3c4770fd914216
edmanf <edmanfeng@hotmail.com>
2019-06-20 14:22:37 -0700

    Changed companion object to top-level object

--- app/src/main/java/edmanfeng/smashnotes/SharedPrefs.kt
+++ app/src/main/java/edmanfeng/smashnotes/SharedPrefs.kt
@@ -1,9 +1,9 @@
 package edmanfeng.smashnotes
 
-class SharedPrefs {
-    companion object {
-        const val GAME_SHARED_PREF_KEY = "edmanfeng.SmashNotes.game"
-        const val CHAR_SHARED_PREF_KEY = "edmanfeng.SmashNotes.character"
-        const val STAGE_SHARED_PREF_KEY = "edmanfeng.SmashNotes.stage"
-    }
+object SharedPrefs{
+    const val GAME_SHARED_PREF_KEY = "edmanfeng.SmashNotes.game"
+    const val CHAR_SHARED_PREF_KEY = "edmanfeng.SmashNotes.character"
+    const val STAGE_SHARED_PREF_KEY = "edmanfeng.SmashNotes.stage"
+    const val GAME_TYPE_SHARED_PREF_KEY = "edmanfeng.SmashNotes.game_type"
+    const val GROUP_TYPE_SHARED_PREF_KEY = "edmanfeng.SmashNotes.group_type"
 }
